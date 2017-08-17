b35ae0c47d8ea40c8e241336ddc7f2b1868cc449
Joey Cluett <joesunglassescool@gmail.com>
2017-08-17 02:20:47 -0500

    SimpleCube demo with minecraft textures

--- TextureMapping/main.cpp
+++ TextureMapping/main.cpp
@@ -26,6 +26,8 @@
 
 // Model(s) in CustomLibs
 #include <GL_Utils/Model/Panel.h>
+#include <GL_Utils/Model/SimpleCube.h>
+
 #include <MISC/TimeClass.h>
 #include "proj-tool.h"
 
@@ -41,30 +43,37 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
     // initialization functions
-    GLFWwindow* window = GLFW_Init(window_width, window_height, "TextureMapping", 3, 3, GL_TRUE);
+    GLFWwindow* window = GLFW_Init(window_width, window_height, "TextureMapping", 3, 3, GL_TRUE, GL_FALSE);
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
     TextureLoader::InitIL(); // allows building of textures later on
 
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LESS);
-    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+    glClearColor(0.0f, 0.0f, 0.2f, 1.0f);
 
     string res_loc("../assets/shader/"); // prefix for shaders
-    //Util::Shader shader_texture(res_loc, "vtx_texture.glsl", "frag_texture.glsl");
-    Util::Shader shader_texture(res_loc, "vtx-track-intersect.glsl", "frag-track-intersect.glsl");
+    Util::Shader shader_texture(res_loc, "vtx_texture.glsl", "frag_texture.glsl");
+    //Util::Shader shader_texture(res_loc, "vtx-track-intersect.glsl", "frag-track-intersect.glsl");
 
     string img_loc("../assets/img/");    // prefix for images
-    Util::Texture IMG_grass(  img_loc, "grass-1.jpg");
+    //Util::Texture IMG_grass(  img_loc, "grass-1.jpg");
+    Util::Texture IMG_grass(  img_loc, "minecraft-dirt-top.png");
     Util::Texture IMG_weed(   img_loc, "shaggy-this-isnt-weed.jpg");
     Util::Texture IMG_chicken(img_loc, "is-that-chicken.jpg");
     Util::Texture IMG_os(     img_loc, "os_users_as_seen_by.jpg");
     Util::Texture IMG_brakes( img_loc, "oil-your-brakes.jpg");
     Util::Texture IMG_ground( img_loc, "ndsu_map.jpg");
 
+    Util::Texture IMG_minecraft_top (img_loc, "minecraft-dirt-top.png");
+    Util::Texture IMG_minecraft_side(img_loc, "minecraft-dirt-side.jpg");
+    Util::Texture IMG_minecraft_bott(img_loc, "minecraft-dirt-bottom.jpg");
+
     // needed for Model/View/Projection transformation
     glm::mat4 Projection = glm::perspective(glm::radians(60.0f), ASPECT_RATIO, 0.1f, 100.0f);
 
     Util::FloatCam fCamera(
-            glm::vec3(0, 1, 3),
+            glm::vec3(0, 2, 3),
             6.0,           // movement speed
             window_width,  // window width
             window_height, // window height
@@ -82,33 +91,21 @@ int main(int argc, char* argv[]) {
 
     Model::Panel ground_panel;
     ground_panel.setRotation(M_PI/-2.0f, 1, 0, 0);
-    ground_panel.setTranslation(MAP_SCALE_X/2, 0, MAP_SCALE_Y/2);
+    //ground_panel.setTranslation(MAP_SCALE_X/2, 0, MAP_SCALE_Y/2);
     ground_panel.setScale(MAP_SCALE_X, MAP_SCALE_Y, 1);
     ground_panel.setShaderTexture(shader_texture, IMG_ground);
     ground_panel.setUVCoordinates(ndsu_uv);
     ground_panel.finalize();
 
-    Model::Panel panel_1;
-    panel_1.setTranslation(0, 0.5, 0.5);
-    panel_1.setShaderTexture(shader_texture, IMG_chicken);
-    panel_1.finalize();
 
-    Model::Panel panel_2;
-    panel_2.setRotation(M_PI / 2.0, 0, 1, 0);
-    panel_2.setTranslation(0.5, 0.5, 0);
-    panel_2.setShaderTexture(shader_texture, IMG_weed);
-    panel_2.finalize();
-
-    Model::Panel panel_3;
-    panel_3.setTranslation(0, 0.5, -0.5);
-    panel_3.setShaderTexture(shader_texture, IMG_grass);
-    panel_3.finalize();
-
-    Model::Panel panel_4;
-    panel_4.setRotation(M_PI / -2.0, 0, 1, 0);
-    panel_4.setTranslation(-0.5, 0.5, 0);
-    panel_4.setShaderTexture(shader_texture, IMG_brakes);
-    panel_4.finalize();
+    Model::SimpleCube cube_1(10, 10, 10);
+    cube_1.applyTranslation(0, 8, 0);
+    cube_1.setTranslation(0, 0, 0);
+    cube_1.setShader(shader_texture);
+    cube_1.setTextureTop(IMG_minecraft_top);
+    cube_1.setTextureSides(IMG_minecraft_side);
+    cube_1.setTextureBottom(IMG_minecraft_bott);
+    cube_1.finalize();
 
     TimeClass tc;
 
@@ -116,8 +113,13 @@ int main(int argc, char* argv[]) {
         glm::vec3 cPos = fCamera.getPosition();
         glm::vec3 cDir = fCamera.getDirection();
 
+        double delta_time = tc.getElapsedSecondsUpdate();
+        static double cube_rot = 0.0;
+        cube_rot += delta_time;
+        cube_1.setRotation(cube_rot, 1, 0, 0);
+
         if(cPos.y > 0.1)
-            fCamera.update((float)tc.getElapsedSecondsUpdate());
+            fCamera.update((float)delta_time);
         else
             fCamera.setPosition(glm::vec3(cPos.x, 0.15, cPos.z));
 
@@ -128,20 +130,14 @@ int main(int argc, char* argv[]) {
         glm::mat4 View = fCamera.getTf();
 
         ground_panel.setViewProjection(View, Projection);
-        panel_1.setViewProjection(View, Projection);
-        panel_2.setViewProjection(View, Projection);
-        panel_3.setViewProjection(View, Projection);
-        panel_4.setViewProjection(View, Projection);
+        cube_1.setViewProjection(View, Projection);
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        // render a sphere where the line intersects the ground
+        // TODO: render a sphere where the line intersects the ground
 
         ground_panel.render();
-        //panel_1.render();
-        //panel_2.render();
-        //panel_3.render();
-        //panel_4.render();
+        cube_1.render();
 
         glfwSwapBuffers(window);
         glfwPollEvents();
